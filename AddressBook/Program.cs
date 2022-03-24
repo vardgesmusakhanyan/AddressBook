9b7265c7caa06b232422e35b30853688c5e549c4
vardgesmusakhanyan <37668878+vardgesmusakhanyan@users.noreply.github.com>
2022-03-24 18:47:03 +0400

    Update Program.cs

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -15,14 +15,6 @@ namespace AddressBook
             cbook.AppInterface();
 
 
-            /*cbook.CreateContact("Vardges", "Musakhanyan", "091453255", "danoville@gmail.com", "Mikoyan");
-            cbook.CreateContact("Hrach", "Asatryan", "077314356", "hrachas48@gmail.com", "Hakob Hakobyan");
-            cbook.CreateContact("Mery", "Grigoryan", "098865665", "17 kvartal xz poxocy");
-            cbook.CreateContact("Tigran", "Hovhannisyan", "098755712", "tigranhovhannisyanisnotavailable@gmail.com", "Exvardi trass");
-            cbook.CreateContact("Anna", "Asatryan", "098979954", "anna.as@gmail.com", "Amerikaner, evropaner, hopefully hayastanner someday");
-            cbook.CreateContact("Mesrop", "Andreasyan", "091009511", "mesand@gmail.com", "6rd masiv");
-            cbook.CreateContact("Vardan", "Malkhasyan", "097777777", "danoville@gmail.com", "Mikoyan");*/
-
             cbook.ShowContacts();
 
 
